96c183b1ddd46be2c061b9d56ef98b9dd2841053
ulasonmez <ulasonmez@outlook.com>
2022-02-09 01:42:21 +0300

    rastgele kelimeler

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import random
 import functions
 from termcolor import colored
-functions.besHarfTxtOlustur()
-# 0 yok 1 doğru yerde 2 yanlış yerde
-"""
-while kelimen!=wordle_kelime:
-    count += 1
-    for i in range(0,len(kelimen)):
-        if kelimen[i] not in wordle_kelime:
-            functions.harfYok(kelimen[i])
-        else:
-            #aynı yer
-            if(kelimen[i] == wordle_kelime[i]):
-                functions.dogruYerde(kelimen[i],i)
-            #farklı yerdeyse
+toplam = 0
+for i in range(0,500):
+    functions.besHarfTxtOlustur()
+    wordle_kelimesi = random.choice(open('5harfkelimeler.txt', encoding="utf8").read().splitlines())
+    dogruluk = ""
+    kelimen = random.choice(open('5harfkelimeler.txt', encoding="utf8").read().splitlines())
+    count = 1
+    while dogruluk != '11111':
+        count += 1
+        dogruluk = ""
+        for i in range(0, 5):
+            if kelimen[i] not in wordle_kelimesi:
+                dogruluk += "0"
             else:
-                functions.dogruYerdeDegil(kelimen[i],i)
+                if kelimen[i] == wordle_kelimesi[i]:
+                    dogruluk += "1"
+                else:
+                    dogruluk += "2"
+        if len(dogruluk) == 5:
+            for i in range(0, 5):
+                if dogruluk[i] == '0':
+                    functions.harfYok(kelimen[i])
+                elif dogruluk[i] == '1':
+                    functions.dogruYerde(kelimen[i], i)
+                elif dogruluk[i] == '2':
+                    functions.dogruYerdeDegil(kelimen[i], i)
+        kelimen = random.choice(open('5harfkelimeler.txt', encoding="utf8").read().splitlines())
 
-    kelimen = random.choice(open('5harfkelimeler.txt', encoding="utf8").read().splitlines())
-"""
-dogruluk = ""
-kelimen = random.choice(open('5harfkelimeler.txt',encoding="utf8").read().splitlines())
-print(kelimen)
-while dogruluk != '11111':
-    dogruluk = str(input('dogruluk giriniz'))
-    if len(dogruluk) == 5:
-        for i in range(0,5):
-            if dogruluk[i] == '0':
-                functions.harfYok(kelimen[i])
-            elif dogruluk[i] == '1':
-                functions.dogruYerde(kelimen[i],i)
-            elif dogruluk[i] == '2':
-                functions.dogruYerdeDegil(kelimen[i],i)
-    kelimen = random.choice(open('5harfkelimeler.txt', encoding="utf8").read().splitlines())
-    print(kelimen)
-
-print(colored(kelimen,"green"))
+    print(colored(f'{count} denemede bulunan kelime = {kelimen}', "green"))
+    toplam += count
+print(f'toplam = {toplam}, ortalama = {toplam/100}')
 
